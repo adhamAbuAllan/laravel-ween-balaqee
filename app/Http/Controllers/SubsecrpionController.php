b80e57f2954ba7701eccc2a41ce3a00c2a45e4ee
needf <needforspeedun3@gmail.com>
2023-11-28 21:30:31 +0200

    commit 11/28/2023 change the url of duc photos

--- app/Http/Controllers/SubsecrpionController.php
+++ app/Http/Controllers/SubsecrpionController.php
@@ -76,7 +76,7 @@ class SubsecrpionController extends Controller
             $path = $dir . '/'
                 .
                 $imageName;
-            $quitUrl = 'https://weenbalaqee.com/laravel-ween-balaqee/' . $path;
+            $quitUrl = 'https://weenbalaqee.com/laravel-ween-balaqee' . $path;
             $data = Subsecrpion::create([
                 'documentary_photo' => $quitUrl,
                 'payment_status'=>$payment_status,
