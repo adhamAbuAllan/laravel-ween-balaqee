185c3af5a0ce0711651bd4be85e36b55386a4b8e
needf <needforspeedun3@gmail.com>
2023-12-15 05:58:42 +0200

    the last update 12/16/2023, comment. change the phone length from 255 to 9

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('phone')->unique();
+            $table->string('phone',9)->unique();
 //            $table->string('about_the_user')->default("");
 //            $table->string('major',55)->default("");
 //            $table->string('email')->default("")->unique();
