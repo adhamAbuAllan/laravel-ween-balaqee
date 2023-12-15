34eead4dd580e36e2ea547ce381a0c0f47187a11
needf <needforspeedun3@gmail.com>
2023-12-15 03:46:40 +0200

    the last update 12/16/2023, comment. change country_phone_number field from int type to string type

--- database/migrations/2023_12_14_232546_create_country_phone_numbers_table.php
+++ database/migrations/2023_12_14_232546_create_country_phone_numbers_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('country_phone_numbers', function (Blueprint $table) {
             $table->id();
             $table->string('country_name',17);
-            $table->integer('country_phone_number')->unique();
+            $table->string('country_phone_number',3)->unique();
             $table->timestamps();
         });
     }
